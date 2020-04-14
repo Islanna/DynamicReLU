1ee4394be4c9e5517d7dc2e5605416b65b321e53
Islanna <slizhikova.a.v@gmail.com>
2020-04-14 12:15:13 +0300

    Add Conv2d support

--- dyrelu.py
+++ dyrelu.py
@@ -2,29 +2,45 @@ import torch
 from torch import nn
 
 
-class DyReLUA(nn.Module):
-    def __init__(self, channels, reduction=4, k=2):
-        super(DyReLUA, self).__init__()
+class DyReLU(nn.Module):
+
+    def __init__(self, channels, reduction=4, k=2, conv_type='2d'):
+        super(DyReLU, self).__init__()
         self.channels = channels
         self.k = k
+        self.conv_type = conv_type
+        assert self.conv_type in ['1d', '2d']
 
         self.fc1 = nn.Linear(channels, channels // reduction)
         self.relu = nn.ReLU(inplace=True)
         self.fc2 = nn.Linear(channels // reduction, 2*k)
         self.sigmoid = nn.Sigmoid()
 
-        self.channels = channels
         self.register_buffer('lambdas', torch.Tensor([1.]*k + [0.5]*k).float())
         self.register_buffer('init_v', torch.Tensor([1.] + [0.]*(2*k - 1)).float())
 
-    def forward(self, x):
-        assert x.shape[1] == self.channels
-        # BxCxL
+    def get_theta(self, x):
         theta = torch.mean(x, axis=-1)
+        if self.conv_type == '2d':
+            theta = torch.mean(theta, axis=-1)
         theta = self.fc1(theta)
         theta = self.relu(theta)
         theta = self.fc2(theta)
         theta = 2 * self.sigmoid(theta) - 1
+        return theta
+
+    def forward(self, x):
+        raise NotImplementedError
+
+
+class DyReLUA(DyReLU):
+    def __init__(self, channels, reduction=4, k=2, conv_type='2d'):
+        super(DyReLUA, self).__init__(channels, reduction, k, conv_type)
+        self.fc2 = nn.Linear(channels // reduction, 2*k)
+
+    def forward(self, x):
+        assert x.shape[1] == self.channels
+        theta = self.get_theta(x)
 
         relu_coefs = theta.view(-1, 2*self.k) * self.lambdas + self.init_v
         # BxCxL -> LxCxBx1
@@ -32,36 +48,33 @@ class DyReLUA(nn.Module):
         output = x_perm * relu_coefs[:, :self.k] + relu_coefs[:, self.k:]
         # LxCxBx2 -> BxCxL
         result = torch.max(output, dim=-1)[0].transpose(0, -1)
+
         return result
 
 
-class DyReLUB(nn.Module):
-    def __init__(self, channels, reduction=4, k=2):
-        super(DyReLUB, self).__init__()
-        self.channels = channels
-        self.k = k
-
-        self.fc1 = nn.Linear(channels, channels // reduction)
-        self.relu = nn.ReLU(inplace=True)
+class DyReLUB(DyReLU):
+    def __init__(self, channels, reduction=4, k=2, conv_type='2d'):
+        super(DyReLUB, self).__init__(channels, reduction, k, conv_type)
         self.fc2 = nn.Linear(channels // reduction, 2*k*channels)
-        self.sigmoid = nn.Sigmoid()
-
-        self.register_buffer('lambdas', torch.Tensor([1.]*k + [0.5]*k).float())
-        self.register_buffer('init_v', torch.Tensor([1.] + [0.]*(2*k - 1)).float())
 
     def forward(self, x):
         assert x.shape[1] == self.channels
-        # BxCxL
-        theta = torch.mean(x, axis=-1)
-        theta = self.fc1(theta)
-        theta = self.relu(theta)
-        theta = self.fc2(theta)
-        theta = 2 * self.sigmoid(theta) - 1
+        theta = self.get_theta(x)
 
         relu_coefs = theta.view(-1, self.channels, 2*self.k) * self.lambdas + self.init_v
-        # BxCxL -> LxBxCx1
-        x_perm = x.permute(2, 0, 1).unsqueeze(-1)
-        output = x_perm * relu_coefs[:, :, :self.k] + relu_coefs[:, :, self.k:]
-        # LxBxCx2 -> BxCxL
-        result = torch.max(output, dim=-1)[0].permute(1, 2, 0)
+
+        if self.conv_type == '1d':
+            # BxCxL -> LxBxCx1
+            x_perm = x.permute(2, 0, 1).unsqueeze(-1)
+            output = x_perm * relu_coefs[:, :, :self.k] + relu_coefs[:, :, self.k:]
+            # LxBxCx2 -> BxCxL
+            result = torch.max(output, dim=-1)[0].permute(1, 2, 0)
+
+        elif self.conv_type == '2d':
+            # BxCxHxW -> HxWxBxCx1
+            x_perm = x.permute(2, 3, 0, 1).unsqueeze(-1)
+            output = x_perm * relu_coefs[:, :, :self.k] + relu_coefs[:, :, self.k:]
+            # HxWxBxCx2 -> BxCxHxW
+            result = torch.max(output, dim=-1)[0].permute(2, 3, 0, 1)
+
         return result
